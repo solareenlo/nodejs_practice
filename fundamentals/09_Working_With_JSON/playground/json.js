1448cc37d34848aa7f779677206bc5e2a0184cd0
solareenlo <solareenlo@gmail.com>
2018-11-11 22:48:58 +0900

    JSON.stringifyでobject->string, JSON.parseでstring->object

---
+++ fundamentals/09_Working_With_JSON/playground/json.js
@@ -0,0 +1,33 @@
+let obj = {
+  name: 'solareenlo'
+};
+
+console.log(typeof obj);
+console.log(obj);
+
+// objectをstringにしてくれる
+let stringObj = JSON.stringify(obj);
+console.log(typeof stringObj);
+console.log(stringObj);
+
+// stringをobjectにしてくれる
+let personString = '{"name": "Andrew", "age": "25"}';
+let person = JSON.parse(personString);
+console.log(typeof person);
+console.log(person);
+
+
+
+const fs = require('fs');
+
+let originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+};
+let originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+let noteString = fs.readFileSync('notes.json');
+let note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
