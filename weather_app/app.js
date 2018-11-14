10be544ee3324f13118db320959d52f8785c442c
solareenlo <solareenlo@gmail.com>
2018-11-15 00:03:22 +0900

    Forecast.ioさんから温度情報をapiでとりあえず取得

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,25 +1,46 @@
+// const request = require('request');
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode');
+// const weather = require('./weather/weather');
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// let lati_long = 1;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//     lati_long = results;
+//   }
+// });
+//
+// console.log(lati_long);
+
 const request = require('request');
-const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode');
-
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
-
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+request({
+  url: `https://api.darksky.net/forecast/your_forecast_api_key/35.6894875,139.6917064`,
+  json: true
+}, (error, response, body) => {
+  if (error) {
+    console.log('Unable to connect to Forecast.io server.');
+  } else if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('Unable to fetch weather.');
   }
 });
---
+++ weather_app/weather/weather.js
@@ -0,0 +1,6 @@
+const request = require('request');
+
+const myKey = 'f5e72f17f9fd06c527530e6d6ec0e2ed';
+
+let weather = (address, callback) => {
+};
