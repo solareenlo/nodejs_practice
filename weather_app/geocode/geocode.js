280e9f79918c3cb3c35010ea53d607e729024a29
solareenlo <solareenlo@gmail.com>
2018-11-14 22:03:52 +0900

    コールバック関数を用いて、緯度経度の情報を取得する操作を、別ファイルにする

---
+++ weather_app/geocode/geocode.js
@@ -0,0 +1,26 @@
+const request = require('request');
+
+const myKey = 'your_google_api_key';
+
+let geocodeAddress = (address, callback) => {
+  let encodeAddress = encodeURIComponent(address);
+
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}&key=${myKey}`,
+    json: true
+  }, (error, reqponse, body) => {
+    if (error) {
+      callcack('Unable to connect to mapquest servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+      callback('Unable to find that address.');
+    } else if (body.status === 'OK') {
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng
+      });
+    }
+  });
+};
+
+module.exports.geocodeAddress = geocodeAddress;
