6fe5be978dbce5e6f764b6649adb8687b9a4ee61
solareenlo <solareenlo@gmail.com>
2018-11-14 19:05:38 +0900

    コンソールから入力したアドレスの緯度経度を表示

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,11 +1,27 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+let encodeAddress = encodeURIComponent(argv.address);
 
 request({
-  url: 'http://www.mapquestapi.com/geocoding/v1/address?key=yourKey&location=1301%20lombard%20street%20philadelphia',
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=yourKey&location=${encodeAddress}`,
   json: true
 }, (error, reqponse, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
-  console.log(body);
+  // console.log(JSON.stringify(body, undefined, 2));
+  // console.log(body);
   console.log(`Address: ${body.results[0].providedLocation.location}`);
   console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
   console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
