01882ce6fa70156cca1756d25f3ff46550fc9b55
solareenlo <solareenlo@gmail.com>
2018-11-15 16:38:29 +0900

    promiseの入れ子にして、２数の和を２回実行

--- weather_app/playground/promise.js
+++ weather_app/playground/promise.js
@@ -12,6 +12,11 @@ let asyncAdd = (a, b) => {
 
 asyncAdd(5, 7).then((res) => {
   console.log('Result: ', res);
+  return asyncAdd(res, 33);
+}, (errorMessage) => {
+  console.log(errorMessage);
+}).then((res) => {
+  console.log('Should be 45', res);
 }, (errorMessage) => {
   console.log(errorMessage);
 });
