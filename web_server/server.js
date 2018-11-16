ba0b556e127cc6cbd354a026eb1585cc0de46aff
solareenlo <solareenlo@gmail.com>
2018-11-16 11:36:49 +0900

    hbsの機能を使って、同じ操作であるfooterとheaderを別ファイルにする

--- web_server/server.js
+++ web_server/server.js
@@ -3,21 +3,29 @@ const hbs = require('hbs');
 
 let app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (request, response) => {
   response.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website.',
-    currentYear: new Date().getFullYear()
+    wellcomeMessage: 'Welcome to my website.'
   });
 });
 
 app.get('/about', (request, response) => {
   response.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
