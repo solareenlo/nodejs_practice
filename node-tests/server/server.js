d97840ff8f40e29ab07e5c760de47efa9de2592d
solareenlo <solareenlo@gmail.com>
2018-11-16 23:33:25 +0900

    localhost:3000/users にオブジェクト情報を表示

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -3,7 +3,25 @@ const express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.send([{
+    name: 'Taro',
+    age: 27
+  }, {
+    name: 'sola',
+    age: 25
+  }, {
+    name: 'Jiro',
+    age: 26
+  }]);
 });
 
 app.listen(3000);
+
+module.exports.app = app;
