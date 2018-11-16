c57aa72fc8b7791f270a91e5b287b7853cd9c1e1
solareenlo <solareenlo@gmail.com>
2018-11-16 17:21:32 +0900

    mochaを使った、npm testの練習

---
+++ node-tests/utils/utils.test.js
@@ -0,0 +1,17 @@
+const utils = require('./utils');
+
+it('should add two numbers', () => {
+  let res = utils.add(33, 11);
+
+  if (res !== 44) {
+    throw new Error(`Expected 44, but got ${res}.`);
+  }
+});
+
+it('should square a number', () => {
+  let res = utils.square(2);
+
+  if (res !== 4) {
+    throw new Error(`Expected 4, but got ${res}.`);
+  }
+});
