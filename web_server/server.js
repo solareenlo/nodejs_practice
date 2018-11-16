2962d37fab7bfe3a87af33fdfc05b05de8a0ba0d
solareenlo <solareenlo@gmail.com>
2018-11-16 12:14:08 +0900

    メンテナンスページを実装

--- web_server/server.js
+++ web_server/server.js
@@ -7,7 +7,7 @@ let app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+
 app.use((request, response, next) => {
   let now = new Date().toString();
   let log = `${now}: ${request.method} ${request.url}`;
@@ -19,6 +19,12 @@ app.use((request, response, next) => {
   });
   next();
 });
+
+app.use((request, response, next) => {
+  response.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
