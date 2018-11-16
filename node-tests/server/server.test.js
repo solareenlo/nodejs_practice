d914bd18ace36a7433af4fbf23bd0bcda1944fb2
solareenlo <solareenlo@gmail.com>
2018-11-16 23:46:31 +0900

    describeを使って, mocha **/*.test.js時の内容を見やすくする

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,27 +3,34 @@ const expect = require('expect');
 
 let app = require('./server').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
+describe('Server', () => {
+  describe('GET /', () => {
+    it('should return hello world response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
+  });
+
+  describe('GET /users', () => {
+    it('should return my user object', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            name: 'sola',
+            age: 25
+          });
+        })
+      .end(done);
+    });
+  });
 });
 
-it('should return my user object', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'sola',
-        age: 25
-      });
-    })
-  .end(done);
-});
