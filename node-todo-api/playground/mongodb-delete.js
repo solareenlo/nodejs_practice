5351d54c404a516adfa9656bcebdcc49a744425c
solareenlo <solareenlo@gmail.com>
2018-11-17 21:32:15 +0900

    mongodbのデータを, 1度にたくさんdelete, 1つのデータをdelete, 1つのデータをfindしてからdeleteする

---
+++ node-todo-api/playground/mongodb-delete.js
@@ -0,0 +1,29 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server.');
+  }
+  console.log('Connected to MongoDB server.');
+  const db = client.db('TodoApp');
+
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat luch'}).then((result) => {
+  //   console.log(result);
+  // }, (err) => {
+  //   console.log(`Unable to delete.`);
+  // });
+
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').deleteMany({name: 'sola'});
+  client.close();
+});
