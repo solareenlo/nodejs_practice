73a1693962eb99a33b3d9eadb1ede203124877aa
solareenlo <solareenlo@gmail.com>
2018-11-17 18:01:58 +0900

    mongodbにデータをinsertする

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -10,22 +10,22 @@ console.log(obj);
 // let {name} = user;
 // console.log(name);
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (err, client) => {
   if (err) {
     return console.log('unable to connect to MongoDB server.');
   }
   console.log('Connected to MongoDB serber.');
   const db = client.db('TodoApp');
 
-  // db.collection('Todos').insertOne({
-  //   text: 'Something to do',
-  //   completed: false
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log('Unable to insert todo', err);
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // })
+  db.collection('Todos').insertOne({
+    text: 'Something to do',
+    completed: false
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert todo', err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  })
 
   // db.collection('Users').insertOne({
   //   name: 'sola',
