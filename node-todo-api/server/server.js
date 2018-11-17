33be3bc621f16451a30894f6762b65d940be9dee
solareenlo <solareenlo@gmail.com>
2018-11-18 08:40:03 +0900

    server.jsを リファクタリングしてファイル分割

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,27 +1,3 @@
-let mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-let User = mongoose.model('User', {
-  user: {
-    type: String
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    minlength: 1
-  }
-});
-
-let user = new User({
-  user: 'sola',
-  email: ' sola@example.com '
-});
-
-user.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (err) => {
-  console.log('Unable to save', err);
-});
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
