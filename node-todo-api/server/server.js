92652016196f64e0af012ded24f139abb0926438
solareenlo <solareenlo@gmail.com>
2018-11-19 02:30:27 +0900

    idでDBに問い合わせる時のテスト段階を実装

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -32,9 +32,11 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
   let id = req.params.id;
+
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
+
   Todo.findById(id).then((todo) => {
     if (!todo) {
       return res.status(404).send();
