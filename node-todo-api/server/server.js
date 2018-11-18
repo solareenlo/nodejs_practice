67604bba235730c1407c0cf6cb24005a81145906
solareenlo <solareenlo@gmail.com>
2018-11-18 10:27:03 +0900

    postmanからjsonデータをmongodbに投入

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,26 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  // console.log(req.body);
+  let todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on Port 3000.');
+});
